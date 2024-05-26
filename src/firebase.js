00c393ac2947044d8e3ac88104050f6633ce231d
erajneeshverma <rajneeshverma94306@gmail.com>
2024-05-26 18:34:43 +0530

    Client Page

---
+++ src/firebase.js
@@ -0,0 +1,55 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD3UsehNLP4TeZU-CIBUzK8mnbRtmelmBo",
+//   authDomain: "fir-chat-app-1838f.firebaseapp.com",
+//   projectId: "fir-chat-app-1838f",
+//   storageBucket: "fir-chat-app-1838f.appspot.com",
+//   messagingSenderId: "998335363166",
+//   appId: "1:998335363166:web:61941f3d03bb442a281ccf"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3UsehNLP4TeZU-CIBUzK8mnbRtmelmBo",
+  authDomain: "fir-chat-app-1838f.firebaseapp.com",
+  projectId: "fir-chat-app-1838f",
+  storageBucket: "fir-chat-app-1838f.appspot.com",
+  messagingSenderId: "998335363166",
+  appId: "1:998335363166:web:ce3c3c630baa3a68281ccf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
